1654a28c5eb7a38f947845d9ec707ac4be942ec2
NovaEBA <63096721+NovaEBA@users.noreply.github.com>
2023-03-08 16:49:03 +0100

    Initial commit

---
+++ Service/InstallationService.php
@@ -0,0 +1,40 @@
+<?php
+
+// src/Service/InstallationService.php
+namespace CommonGateway\PetStoreBundle\Service;
+
+use App\Entity\DashboardCard;
+use App\Entity\Endpoint;
+use CommonGateway\CoreBundle\Installer\InstallerInterface;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class InstallationService implements InstallerInterface
+{
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    public function install()
+    {
+        $this->checkDataConsistency();
+    }
+
+    public function update()
+    {
+        $this->checkDataConsistency();
+    }
+
+    public function uninstall()
+    {
+        // Do some cleanup
+    }
+
+    public function checkDataConsistency()
+    {
+        $this->entityManager->flush();
+    }
+}
