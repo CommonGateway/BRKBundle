8f9cce856ce9f094b000cf8e11e0431803137751
Robert Zondervan <robert@conduction.nl>
2023-04-21 13:03:30 +0200

    remove bracket

--- src/ActionHandler/SnapshotHandler.php
+++ src/ActionHandler/SnapshotHandler.php
@@ -52,7 +52,6 @@ class SnapshotHandler implements ActionHandlerInterface
             'description' => 'This handler converts a single snapshot into multiple BRK objects',
             'required'    => [],
             'properties'  => [],
-        ],
         ];
 
     }//end getConfiguration()
